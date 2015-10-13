298cebbff903561df56a3e74dac2c162513c4556
Boris Marinov <boris.marinov@tick42.com>
2015-10-13 18:32:52 +0300

    Added the Lift operation / code cleanup

--- test/basic.js
+++ test/basic.js
@@ -1,32 +1,49 @@
 var sonne = require('../lib/main')
 var sinon = require('sinon')
 module.exports = {
-  maybeId:{
-    setUp:function(done){
-      this.maybeId = sonne.make(sonne.data.maybe, sonne.data.id)
-      this.spy = sinon.spy((a) => a)
-      done()
-    },
-    map:function (test) {
-      maybeId = this.maybeId
-
-      this.maybeId(4)
+    maybeId:function (test) {
+      var maybeId = sonne.make(sonne.data.maybe, sonne.data.id)
+      var mapSpy = sinon.spy((a) => a)
+      var flatMapSpy = sinon.spy((a) => a)
+      maybeId(4)
         .map(function (val) {return val + 1})
-        .map(function (a) {test.equals(a, 5, a);return a})
+        .map(function (a) {test.equals(a, 5, 'A call to "map" modifies the value, and packs it again');return a})
         .map(function (num) {return undefined})
-        .map(this.spy)
-      test.equals(this.spy.called, false)
-      test.done()
-    },
-    flatMap:function(test){ 
-      this.maybeId(4)
+        .map(mapSpy)
+      test.equals(mapSpy.called, false, "After a val is set to undefined, functions are no longer called")
+
+      maybeId(4)
         .flatMap(function (val) {
           return maybeId(5)
         })
-        .map(this.spy)
-      test.equals(this.spy.firstCall.returnValue, 5)
+        .map(flatMapSpy)
+      test.equals(flatMapSpy.firstCall.returnValue, 5)
+      
+      maybeId(4).liftMaybe({idVal:45})
+
       test.done()
     },
+    
+    idMaybe:function(test){ 
+      var idMaybe = sonne.make(sonne.data.id, sonne.data.maybe)
+      var mapSpy = sinon.spy((a) => a)
+      var flatMapSpy = sinon.spy((a) => a)
+
+      idMaybe(4)
+        .map(function (val) {return val + 1})
+        .map(function (a) {test.equals(a, 5, 'A call to "map" modifies the value, and packs it again');return a})
+        .map(function (num) {return undefined})
+        .map(mapSpy)
+ //     test.equals(mapSpy.called, false, "After a val is set to undefined, functions are no longer called")
+      idMaybe(4)
+        .flatMap(function (val) {
+          return idMaybe(5)
+        })
+        .map(flatMapSpy)
+      test.equals(flatMapSpy.firstCall.returnValue, 5)
+      test.done()
+    }
+
    /* maybeList:{
       setUp:function(done){
         this.maybeList = sonne.make(sonne.data.maybe, sonne.data.list)
@@ -39,4 +56,3 @@ module.exports = {
       
     }*/
   }
-}
