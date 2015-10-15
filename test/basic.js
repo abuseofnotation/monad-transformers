c32198f3e5c63dbcda98147a5d3cfee67a3d9df3
Boris Marinov <boris.marinov@tick42.com>
2015-10-15 17:56:40 +0300

    Fixed issue with lift

--- test/basic.js
+++ test/basic.js
@@ -1,57 +1,45 @@
 var sonne = require('../lib/main')
 var sinon = require('sinon')
+
+var maybeStacks = [sonne.make(sonne.data.id, sonne.data.maybe), sonne.make(sonne.data.maybe, sonne.data.id)]
+
+var monads = [sonne.make(sonne.data.id, sonne.data.maybe), sonne.make(sonne.data.maybe, sonne.data.id)]
+
 module.exports = {
-    maybeId:function (test) {
-      var maybeId = sonne.make(sonne.data.maybe, sonne.data.id)
-      var mapSpy = sinon.spy((a) => a)
-      var flatMapSpy = sinon.spy((a) => a)
-      
-      
-      maybeId(4)
-        .map(function (val) {return val + 1})
-        .liftMaybe((val)=> {
-          test.equals(val, 5, 'A call to "map" modifies the value, and packs it again')
-          return {maybeVal:undefined}
-	})
-        .map(mapSpy)
-      test.equals(mapSpy.called, false, "After a val is set to undefined, functions are no longer called")
-
-      maybeId(4)
-        .flatMap(function (val) {
-          return maybeId(5)
-        })
-        .map(flatMapSpy)
-      test.equals(flatMapSpy.firstCall.returnValue, 5)
-
+    Maybe (test) {
+      maybeStacks.forEach((maybe) =>{
+        var spy = sinon.spy((a) => a)
+        var chainSpy = sinon.spy((a) => a)
+        maybe(4)
+          .map(function (val) {return val + 1})
+          .chainMaybe((val)=> {
+            test.equals(val, 5, 'A call to "map" modifies the value, and packs it again')
+            return {maybeVal:undefined}
+          })
+        .map(spy)
+        test.equals(spy.called, false, "After a val is set to undefined, functions are no longer called")
+      })
       test.done()
     },
-    
-    idMaybe:function(test){ 
-      var idMaybe = sonne.make(sonne.data.id, sonne.data.maybe)
-      var mapSpy = sinon.spy((a) => a)
-      var flatMapSpy = sinon.spy((a) => a)
-      idMaybe(4).flatMap(()=>{maybeVal:undefined})
+    chain (test){
+      const val = 5
+      monads.forEach(monad => {
+        var spy = sinon.spy((a) => a)
+        monad(val)
+         .chain((val)=> monad(val))
+         .chain((val)=> monad(val)._value)
+      .map(spy)	    
+        test.equals(spy.firstCall.returnValue, val, "Unpacking a monad and packing it again yeilds the same structure")
+      }) 
+      test.done()
+    },
+    maybeList (test){
+      var maybeList = sonne.make(sonne.data.maybe, sonne.data.list)
+      test.done()
+    },
+    test (test){
       debugger
-      idMaybe(4)
-        .map(function (val) {return val + 1})
-        .liftMaybe((val) => {
-          test.equals(val, 5, 'A call to "map" modifies the value, and packs it again')
-          return {maybeVal:undefined}
-	})
-        .map(mapSpy)
-      test.equals(mapSpy.called, false, "After a val is set to undefined, functions are no longer called")
       test.done()
     }
 
-   /* maybeList:{
-      setUp:function(done){
-        this.maybeList = sonne.make(sonne.data.maybe, sonne.data.list)
-        this.spy = sinon.spy((a) => a)
-        done()
-      },
-      flatMap:function(test){
-        this.maybeList([1,2,3])
-      }
-      
-    }*/
   }
