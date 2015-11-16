4dc1693931cea8afa92bcec471e10a29c4410e4f
Boris Marinov <marinovboris@gmail.com>
2015-11-16 18:54:20 +0200

    Commited missing files

---
+++ test/permutations.js
@@ -0,0 +1,19 @@
+//This modules allows you to run nodeunit tests on all possible combinations of monads, defined in the library
+var combinatorics = require('js-combinatorics')
+
+const id = require('../lib/id')
+const data = require('../lib/data')
+const comp = require('../lib/comp')
+
+
+debugger
+const monads = [].concat([comp.list, data.maybe, id.id, id.idWrapped, comp.state])
+
+const stacks = combinatorics.permutation(monads, 3).toArray()
+
+module.exports = (stackFilter, testFunction) =>
+  stacks.filter(stackFilter).reduce((obj,stack) => {
+    obj[stack.map(s=>s.name).join('')] = testFunction.apply(null, stack)
+    return obj
+  }, {})
+
