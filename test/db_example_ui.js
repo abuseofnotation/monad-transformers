99b570075136537630084bff2b90a320729e5d8d
Boris Marinov <Boris_Marinov@epam.com>
2016-01-27 14:33:59 +0200

    WIP

--- test/db_example_ui.js
+++ test/db_example_ui.js
@@ -4,46 +4,74 @@ const mtl = require("../lib/main.js")
 if ( global.v8debug ) {
 	global.v8debug.Debug.setBreakOnException()
 }
-const m = mtl.make(mtl.data.maybe, mtl.comp.state)
+
+
+const react = {
+  name: 'React',
+  of (val) {
+    return this.outer.of(val)
+  },
+  chain (fn, val) {
+    return this.outer.chain(fn, val)
+  },
+  lift (val) {
+    return val
+  },
+  fold (value, val) {
+    return value(val)
+  },
+  render(val) {
+    debugger 
+    return this.outer.of(val)
+  }
+}
+
+
+
+
+const m = mtl.make(mtl.data.maybe, mtl.comp.state, react)
 
 const immutable = require('immutable')
 
-const val = m.of().mapState( () =>[immutable.List(),  ""])
-const onUserAction = (a) => { val = val.andThen(actions[a.type](a)); val.run() }
+const val = m.of(immutable.List()).setState('')
+const onUserAction = (a) => { val = val.chain(actions[a.type](a)); val.run() }
 
 const checkNotEmpty = (val) => val !== "" ? m.of(val): m.nothing()
 
 const textToNote = (text) => m.of(text).chain(checkNotEmpty).map((text) => ({text:text, done:false}))
-//.andThen(m.of({ text:text, done:false})
+
+const toggle = mtl.curry((index, val) => val.get())
 
 const actions = {
-  type: (text) => m.of(text).mapState((state, text) => [state[0], text]),
-  enter: (_) => m.of().loadState()
-    .chain((state) => m.of(state[1]).chain(textToNote).debug().map((newNote) => [state[0].push(newNote), ''])).saveState(),
-  remove: (i) => m.of(i).mapState((state, index) => [state[0].remove(index), text]),
-  toggle: a => a
+  type: (text) => (val) => m.of(val).setState(text).render(),
+
+  enter: (_) => (val) => m.of().loadState().chain(textToNote).map((newNote) => val.push(newNote)).setState(''),
+
+  remove: (i) => (val) => m.of(val.remove(index)),
+
+  toggle: (i) => (val) => m.of(toggle(i, val))
 }
 
 exports.todo = {
   one (test) {
     val
-      .andThen(actions.type('foo'))
-      .andThen(actions.enter())
+      .chain(actions.type('foo'))
+      .chain(actions.enter())
       .run((val) => {
-        test.equal(val.value.state[0].size, 1)
-        test.equal(val.value.state[1], '')
+        test.equal(val.value.value.size, 1)
+        test.equal(val.value.state, '')
         test.done()
       })
   },
 
   two (test) {
     val
-      .andThen(actions.type('foo'))
-      .andThen(actions.enter())
-      .andThen(actions.type('bar'))
+      .chain(actions.type('foo'))
+      .chain(actions.enter())
+      .chain(actions.type('bar'))
       .run((val) => {
-        test.equal(val.value.state[0].size, 1)
-        test.equal(val.value.state[1], 'bar')
+        test.equal(val.value.value.size, 1)
+        test.equal(val.value.state, 'bar')
         test.done()
       })
   }
