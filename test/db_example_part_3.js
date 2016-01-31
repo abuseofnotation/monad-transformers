b27564ff8854e808626678e4de1b6dfbf9ce2953
Boris Marinov <marinovboris@gmail.com>
2016-01-31 19:59:30 +0200

    examples

--- test/db_example_part_3.js
+++ test/db_example_part_3.js
@@ -19,7 +19,7 @@
  * it. For example functions we call using `map` cannot touch the environment (unless we loaded it 
  * beforehand using `loadEnvironment`).
  * 
- * For our next task - DOM manipulation - we also have an environment on which we act on - a DOM node.
+ * For our next task - IO - we also have an environment on which we act on - the `process`object in node.
  * so we will once again use `Reader`, 
  *
  * Only we are going to modify it just a little bit, so it fits our needs exactly.
@@ -27,9 +27,12 @@
  * We are going to start with the original implementation:
  */
 const mtl = require("../lib/main.js")
-const m = mtl.advanced
-
-var reader = {
+process.stdin.setEncoding('utf8');
+const util = require('util');
+if ( global.v8debug ) {
+	global.v8debug.Debug.setBreakOnException()
+}
+const reader = {
   name: 'Reader',
   //Standard functions:
   of (val) {
@@ -76,14 +79,14 @@ var dom = reader
 /*
  * Change the name, so we don't confuse the two monads while debugging:
  */
-dom.name = 'DOM'
+dom.name = 'StdinStdout'
 /*
- * And lastly, we are also going to patch the `run` method so our new monad grabs its environment
- * a different property of the run context. We do this because we want to be able to use our new
- * monad along with the original Reader and specify different environments.
+ * And lastly, we are also going to patch the `run` method so our new monad uses the `process`
+ * global variable as its environment. We are leaving the possibility to mock our `process`
+ * object if we want to.
  */
 dom.run = function run (f, reader) {
-  return f(reader(this.node))
+  return f(reader(this.process || global.process))
 }
 /*
  * The standars `Reader` helpers provide direct access to the environment to the functions we compose.
@@ -97,58 +100,94 @@ delete dom.loadEnvironment
  * If we wanted to keep them, we would have to change their names, again to avoid conflicts
  * with the original `Reader`.
  *
- * Our DOM monad transformer is done, all we have to do is define some helpers.
+ * Our IO  monad transformer is done, all we have to do is define some helpers that will make it
+ * more usable. We will keep it simple - one method to write in the standard output and one to read
+ * from the standard input.
  *
- * Now that we know that the environment for this reader is going to be a DOM node,
- * we can add some DOM-specific methods to it.
+ * The writing part is trivial.
  *
- * For example we want to have a function accepting a lambda which returns
- * a string and setting the string as the node's `innerHTML`.
- 
- * We can call this one `renderTemplate`, because it renders the value, using the lambda
- * function as a template (notice that `f` is a pure function - has no access to the node)
  */
-dom.renderTemplate = function (f, val) {
-  return (node) => {
-    //Perform the side effect.
-    node.innerHTML =''
-    node.appendChild(f(val)) 
-    //Return the same value.
-    return this.outer.of(val) 
+dom.write = function (f, val) {
+  return (process) => {
+    process.stdout.write(f(val))//Perform side effect
+    return this.outer.of(val)//Return val
   }
 }
 /*
- * Sometimes we may have constructed the node before and may want to just insert it in the DOM
- * directly. It may be worth it to define this as a helper:
+ * The reading part is a bit harder, because the input is asynchronous.
+ * So how do we "return" the input if it is not there yet? 
+ * One way to do it is to return a continuation instead and then
+ * handle this continuation externaly using the task monad. 
+ *
+ * It will look like this:
  */
-dom.render = function (val) {
-  return this.renderTemplate(a => a, val)
-}
-/*
- * We have a function that does output, now we need one that receives input from the DOM.
- * How do we take input from the DOM without having access to the DOM itself?
- * One way to do this is to use the element ID:
- */
-dom.getFieldValue = function (f, val) {
-  return (node) => {
-    //Perform the side effect.
-    const fieldValue = node.getElementById(f(val)).value
-    //Return the same value.
-    return this.outer.of(fieldValue) 
+dom.promptFor = function (f,val) {
+  return (process) => {
+    process.stdout.write(f(val))
+    return this.outer.of((error, success) => {
+      process.stdin.resume()
+      process.stdin.on('data', function (text) {
+        process.stdin.pause()
+        success(util.inspect(text))
+      })
+    })
   }
 }
+/* 
+ * And it can be used like this:
+ */
+const m = mtl.make(mtl.base.task, dom)
+
+const getUsername = () =>
+  m.of()
+    .promptFor(()=> 'Username: ')
+    .chain((usernameContinuation) => m.fromContinuation(usernameContinuation))
+    .write((username) => `Your username is "${username}"`)
+
+/* 
+ * By now you probably know that although the second step is redundant it 
+ * has to be there in order for 
+ * our transformations to act independently of one another, and to be usable
+ * by themselves.
+ *
+ * What you may not know is that they don't have to be independent.
+ *
+ * When several monad transformers are chained, their transformations are 
+ * applied sequentially. This means that each transformation has access to 
+ * and can trigger all previous (or "outer") transformations.
+ *
+ * The ordering in this library is left to right, so if we 
+ * have a stack like `mtl.base.task, dom` then we can use the `Task` monad transformer
+ * in the implementation of the `dom` monad transformer.
+ *
+ * Let's redefine the `promptFor` method so it creates a Task directly:
+ */
+dom.promptFor = function (f,val) {
+  return (process) => {
+    process.stdout.write(f(val))
+    return this.outer.fromContinuation((error, success) => {
+      process.stdin.resume()
+      process.stdin.on('data', function (text) {
+        process.stdin.pause()
+        success(util.inspect(text))
+      })
+    })
+  }
+}
+const mNew = mtl.make(mtl.base.task, dom)
+const getUsernameNew = () =>
+  mNew.of()
+    .promptFor(()=> 'Username: ')
+    .write((username) => `Your username is "${username}"`)
+
+getUsernameNew().run()
 
 /*
- * Let's see how we would use it
- * Now lets write a simple program
+ * What is also cool is that each new transformation "inherits" the methods of the 
+ * previous transformations, which means that the dom transformation will work on any
+ * arbitrary stack, as long as it includes the `Task` transformer.
+ *
+ * For example let's include some transformations that will enable us to run some of
+ * our previous examples:
+ *
  */
-
-const insertField = () =>
-  m.of().chain(() => {
-    div = document.createElement('div')
-    return m
-      .render(div)
-      .cont((error, success) => (div.onClick = success))
-  })
-
-
